069dab0df34a6e1c0c12c0bf5e222df2fe17dd91
João da Silva Correa <joao.da.silva.correa@avanade.com>
2020-01-24 19:07:08 -0300

    ajuste variaveis de ambiente

--- index.js
+++ index.js
@@ -8,11 +8,9 @@ const HapiSwagger = require('hapi-swagger');
 const routes = require('./app/router/index.js');
 
 const init = async () => {
-  console.log(process.env.MODE);
-
   const server = hapi.server({
-    port: process.env.PORT || 80,
-    host: process.env.HOST || '0.0.0.0',
+    port: process.env.MODE === 'production' ? (process.env.PORT || 80) : 3000,
+    host: process.env.MODE === 'production' ? '0.0.0.0' : 'localhost',
   });
 
   const swaggerOptions = {
